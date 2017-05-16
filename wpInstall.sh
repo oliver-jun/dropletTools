9acc7239f079d7850c3685c5c550315c480028b7
Nick <itcarsales@gmail.com>
2017-05-15 21:24:20 -0500

    Add files via upload

--- wpInstall.sh
+++ wpInstall.sh
@@ -188,7 +188,7 @@ NOW=\$(date +%Y%m%d%H%M%S)
 SQL_FILE=\${NOW}_database.sql
 
 # Backup database
-/usr/local/bin/wp db export \$SQL_FILE --add-drop-table
+/usr/local/bin/wp db export \$SQL_FILE --add-drop-table >/dev/null 2>&1
 
 # Compress the database
 gzip \$SQL_FILE
